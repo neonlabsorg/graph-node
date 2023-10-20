89a35bc2e62924b163a8a2edb868588db533f20c
romanova-natasha <zabelina.nattaly@gmail.com>
2023-10-20 10:38:22 +0300

    fix test tag
    fix test tag

--- tests/integration-tests/config/rm_subgraphs.sh
+++ tests/integration-tests/config/rm_subgraphs.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
+TAG=$1
 test_dir=./tests/integration-tests
 
 echo "Removing subgraphs:"
 for entry in "$test_directory/*"
 do
-  var=$(docker exec graph-node-$GITHUB_SHA bash -c "graphman --config /config.toml info $entry-$GITHUB_SHA")
+  var=$(docker exec graph-node-$TAG bash -c "graphman --config /config.toml info $entry-$TAG")
   if [[ $var != "No matches" ]]; then
-    echo "Removing subgraph: $entry-$GITHUB_SHA"
-    docker exec graph-node-$GITHUB_SHA bash -c "yes | graphman --config config.toml drop $entry-$GITHUB_SHA"
+    echo "Removing subgraph: $entry-$TAG"
+    docker exec graph-node-$TAG bash -c "yes | graphman --config config.toml drop $entry-$TAG"
   else
-    echo "Can not find $entry-$GITHUB_SHA subgraph."
+    echo "Can not find $entry-$TAG subgraph."
   fi
   
-  var=$(docker exec graph-node-$GITHUB_SHA bash -c "graphman --config /config.toml info $entry-$GITHUB_SHA")
+  var=$(docker exec graph-node-$TAG bash -c "graphman --config /config.toml info $entry-$TAG")
   if [[ $var =~ "No matches" ]]; then
-    echo "$entry-$GITHUB_SHA subgraph was removed."
+    echo "$entry-$TAG subgraph was removed."
   else
-    echo "Can not remove $entry-$GITHUB_SHA subgraph."
+    echo "Can not remove $entry-$TAG subgraph."
   fi
 done
