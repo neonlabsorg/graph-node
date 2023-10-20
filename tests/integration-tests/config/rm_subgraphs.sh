efc138dd55dfd01ea726ef89fc7acbf647c9950f
romanova-natasha <zabelina.nattaly@gmail.com>
2023-10-20 09:45:20 +0300

    always clear subgraph after tests
    always clear subgraph after tests

--- tests/integration-tests/config/rm_subgraphs.sh
+++ tests/integration-tests/config/rm_subgraphs.sh
@@ -5,8 +5,14 @@ test_dir=./tests/integration-tests
 echo "Removing subgraphs:"
 for entry in "$test_directory/*"
 do
-  echo "Removing subgraph: $entry-$GITHUB_SHA"
-  docker exec graph-node-$GITHUB_SHA bash -c "yes | graphman --config config.toml drop $entry-$GITHUB_SHA"
+  var=$(docker exec graph-node-$GITHUB_SHA bash -c "graphman --config /config.toml info $entry-$GITHUB_SHA")
+  if [[ $var != "No matches" ]]; then
+    echo "Removing subgraph: $entry-$GITHUB_SHA"
+    docker exec graph-node-$GITHUB_SHA bash -c "yes | graphman --config config.toml drop $entry-$GITHUB_SHA"
+  else
+    echo "Can not find $entry-$GITHUB_SHA subgraph."
+  fi
+  
   var=$(docker exec graph-node-$GITHUB_SHA bash -c "graphman --config /config.toml info $entry-$GITHUB_SHA")
   if [[ $var =~ "No matches" ]]; then
     echo "$entry-$GITHUB_SHA subgraph was removed."
